2ef8770a3bdfdb6bad2f58d413faa9f351dcd3f0
PhilipSanzone <philipronsanzone@gmail.com>
2025-01-13 12:21:09 -0500

    C: Added Gameloop, and Player Chara

--- main.py
+++ main.py
@@ -2,17 +2,27 @@
 # the open-source pygame library
 import pygame
 from constants import *
+from player import *
 
 def main():
-    pygame.get_init()
+    pygame.init()
+    clock = pygame.time.Clock() #3
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
+# while loop that draws the games screen    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 100
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
