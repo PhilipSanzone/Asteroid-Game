6e6a136333ced956fd5d9f045afd11af0d6f9773
PhilipSanzone <philipronsanzone@gmail.com>
2025-01-13 13:28:23 -0500

    E: Char is Moveable Now

--- player.py
+++ player.py
@@ -4,20 +4,25 @@ from constants import *
 from main import *
 
 class Player(CircleShape):
-    def __init__(self,x,y,):
+    def __init__(self, x, y):
         self.rotation = 0
         super().__init__(x, y, PLAYER_RADIUS)
 
     def rotate(self,dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
-    def update(self,dt):
+    def update(self, dt):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(+dt)
+
+        if keys[pygame.K_w]:
+            self.move(+dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
 
 # in the player class
     def triangle(self):
@@ -27,3 +32,8 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
